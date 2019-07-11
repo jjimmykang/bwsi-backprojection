af4470cd49fcda5d6a7fa854fdb5802630942640
Jimmy Kang <jimmykang1016@gmail.com>
2019-07-11 14:15:22 -0400

    adjusted command line arguments for ease of use

---
+++ raw_code/pulson440/test.py
@@ -0,0 +1,22 @@
+import argparse
+import datetime
+
+'''
+parser = argparse.ArgumentParser(description='Pulson440 Radar software')
+parser.add_argument('scan_mode', type=str, help='collect or quicklook')
+parser.add_argument('scan_count', type=int, help='Scancount.')
+parser.add_argument('--settings', type=str, help='force a settings file path ')
+parser.add_argument('--create', help='automatically create data file', action='store_true')
+parser.add_argument('--noreturn', help='forces no return data')
+
+# Object to hold command line arguments
+args = parser.parse_args()
+
+if args.settings:
+    print(args.settings)
+else:
+    print('no settings ')
+print(args)
+'''
+
+print(datetime.time)

8f65c92042071fd5bb81da78bb6211e0cd7e99a1
cwu159 <cwu159@gmail.com>
2019-07-16 15:41:51 -0400

    partial vectorization

--- charlie_backproj/backproj.py
+++ charlie_backproj/backproj.py
@@ -23,6 +23,11 @@ with open(datafile_name, 'rb') as file:
 # print()
 # print(data["platform_pos"])
 
+data["scan_data"] = np.asarray(data["scan_data"])
+data["platform_pos"] = np.asarray(data["platform_pos"])
+data["range_bins"] = np.asarray(data["range_bins"])
+
+
 x_loc_real = [6*a/120-3 for a in range(120)]
 y_loc_real = [6*a/120-3 for a in range(120)]
 
@@ -39,12 +44,30 @@ for x in range(len(x_loc_real)) :
         x_loc = x_loc_real[x]
         y_loc = y_loc_real[y]
         z_loc = 0
-        total_dists = []
-        for i in range(len(data["platform_pos"])) :
-            platform_loc = data["platform_pos"][i]
-            total_dists.append(math.sqrt( (platform_loc[0]-x_loc)**2 + (platform_loc[1]-y_loc)**2 + (platform_loc[2]-z_loc)**2 ) )
-            # plane
-            # result = data["range_bins"][0][closest_range_bin - 1]
+
+        total_dists = np.asarray([])
+
+        platform_locs_full = data["platform_pos"]
+        # print("PLATFORM LOCS DIMENSIONS")
+        # print(len(platform_locs_full))
+        # print(len(platform_locs_full[0]))
+        platform_locs_x = platform_locs_full[:,0]
+        platform_locs_y = platform_locs_full[:,1]
+        platform_locs_z = platform_locs_full[:,2]
+        x_locs = np.full(len(platform_locs_x), x_loc)
+        y_locs = np.full(len(platform_locs_y), y_loc)
+        z_locs = np.zeros(len(x_locs))
+        total_dists = np.sqrt( np.square( (platform_locs_x-x_locs) ) + np.square( (platform_locs_y-y_locs) ) + np.square( (platform_locs_z-z_locs) ) )
+        # x = np.square( (platform_locs_x-x_locs) )
+
+        # for i in range(len(data["platform_pos"])) :
+        #     platform_loc = data["platform_pos"][i]
+        #     total_dists.append(math.sqrt( (platform_loc[0]-x_loc)**2 + (platform_loc[1]-y_loc)**2 + (platform_loc[2]-z_loc)**2 ) )
+        #     # plane
+        #     # result = data["range_bins"][0][closest_range_bin - 1]
+        
+        
+        
         range_bins_list = []
         for i in range(len(total_dists)) :
             closest_range_bin_ind = bisect.bisect_left(data["range_bins"][0], total_dists[i]) -1 #TODO: see if its -1 ind or not
@@ -67,5 +90,5 @@ end_time = time.time()
 elapsed_time = end_time-start_time
 print("Done.")
 print("Elapsed time: "+str(elapsed_time))
-# imgplot = plt.imshow(pixels)
-# plt.show()
+imgplot = plt.imshow(pixels, interpolation="gaussian")
+plt.show()

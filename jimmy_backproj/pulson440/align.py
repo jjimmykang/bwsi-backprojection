c72fc6ead7bc40a261c0fa0bcb495ab2df9dc98f
Jimmy Kang <jimmykang1016@gmail.com>
2019-07-29 10:44:15 -0400

    fixed some bugs with match filter

--- jimmy_backproj/pulson440/align.py
+++ jimmy_backproj/pulson440/align.py
@@ -43,18 +43,17 @@ def compute_ranges(file_data):
 
     return ranges_1, ranges_2
 
-def visualize_data(file_data):
+def visualize_data(file_data, title):
 
     scan_data = file_data['scan_data']
     motion_timestamps = file_data['motion_timestamps']
-
-
     print('graphing!')
 
     # Plot
     plt.show()
     image_fig = plt.figure()
     image_ax = image_fig.add_subplot(111)
+    image_fig.suptitle(title, fontsize=20)
     h_img = image_ax.imshow(np.log10(np.abs(scan_data)), extent=(
         file_data['range_bins'][0 ,0],
         file_data['range_bins'][0, -1],
@@ -103,77 +102,15 @@ def graph_signal(img):
     zorder=5)
     plt.show()
 
-def main():
-    # Argument Parser
-    parser = argparse.ArgumentParser(description='align sar data')
-    parser.add_argument('first_cutoff', type=float, help='beginning cutoff in meters')
-    parser.add_argument('last_cutoff', type=float, help='last cutoff in meters')
-    parser.add_argument('--pixel', type=int, help='pixel dimensions each side')
-    parser.add_argument('--window', type=float, help='length of real world window')
-    parser.add_argument('--mode', type=int, help='toggle visualization mode')
-    parser.add_argument('--shift', type=float, help='amount to shift the data by')
-    parser.add_argument('-a', '--automatic', action='store_true', help='toggle automatic mode')
-    args = parser.parse_args()
+def match_filter(entry_data, shift_mode):
 
-    # Command line argument processing
-    graph = args.mode
-
-    if args.pixel == None:
-        pixel_input = 120
-    else:
-        pixel_input = args.pixel
-
-    if not args.window == None:
-        window_input = int(args.window)
-    else:
-        window_input = 6
-
-    if not args.shift == None:
-        align_amt = args.shift
-    else:
-        align_amt = 0
-
-
-    file_data = open_file(MANDRILL_1)
-
-
-
-    # Initialize timestamps
-    # Regularize at the same time
-    motion_timestamps = file_data['motion_timestamps'] - file_data['motion_timestamps'][0]
-    scan_timestamps = file_data['scan_timestamps'] - file_data['scan_timestamps'][0]
-
-    # Import data as variables
-    platform_pos = file_data['platform_pos']
-    scan_data = file_data['scan_data']
-    range_bins = file_data['range_bins']
-    # Set shape
-    scan_data_shape =  file_data['scan_data'].shape
-    # Get the number of scans
+    motion_timestamps = entry_data['motion_timestamps']
+    scan_timestamps = entry_data['scan_timestamps']
+    platform_pos = entry_data['platform_pos']
+    scan_data = entry_data['scan_data']
+    scan_data_shape = scan_data.shape
     num_scans = scan_data_shape[0]
-
-    time_ratio = np.max(motion_timestamps) / np.max(scan_timestamps)
-
-
-    # everything have the same index(same dimensional)
-    ratio = (num_scans * time_ratio) / platform_pos.shape[0]
-    scaled_platform_pos = np.empty((int(num_scans * time_ratio), 3))
-    for i in np.arange(0, 3, 1):
-        scaled_platform_pos[:, i] = np.interp(np.arange(0, int(num_scans * time_ratio), 1) / ratio, np.arange(0, platform_pos.shape[0], 1), platform_pos[:, i])
-
-    motion_timestamps = np.interp(np.arange(0, int(num_scans * time_ratio), 1) / ratio, np.arange(0, motion_timestamps.shape[0], 1), motion_timestamps[:])
-    platform_pos = scaled_platform_pos
-
-
-    entry_data = {'scan_data': scan_data, 'platform_pos': platform_pos,
-        'range_bins': file_data['range_bins'], 'scan_timestamps': scan_timestamps,
-        'motion_timestamps': motion_timestamps, 'corner_reflector_pos': file_data['corner_reflector_pos']
-    }
-
-    if args.automatic == True:
-        shift_mode = 'automatic'
-    else:
-        shift_mode = 'manual'
+    range_bins = entry_data['range_bins']
 
     if shift_mode == 'manual':
         # SHIFTING CODE
@@ -295,23 +232,103 @@ def main():
         platform_pos = platform_pos[index_to_shift:]
         motion_timestamps = regularize(motion_timestamps)
 
-
-
     # Re-regularize(because it got sliced)
     motion_timestamps = regularize(motion_timestamps)
     scan_timestamps = regularize(scan_timestamps)
+    return {'scan_data': scan_data, 'platform_pos': platform_pos,
+        'range_bins': entry_data['range_bins'], 'scan_timestamps': scan_timestamps,
+        'motion_timestamps': motion_timestamps, 'corner_reflector_pos': entry_data['corner_reflector_pos']
+    }
+
+
+
+def main():
+    # Argument Parser
+    parser = argparse.ArgumentParser(description='align sar data')
+    parser.add_argument('first_cutoff', type=float, help='beginning cutoff in meters')
+    parser.add_argument('last_cutoff', type=float, help='last cutoff in meters')
+    parser.add_argument('--pixel', type=int, help='pixel dimensions each side')
+    parser.add_argument('--window', type=float, help='length of real world window')
+    parser.add_argument('--mode', type=int, help='toggle visualization mode')
+    parser.add_argument('--shift', type=float, help='amount to shift the data by')
+    parser.add_argument('-a', '--automatic', action='store_true', help='toggle automatic mode')
+    args = parser.parse_args()
+
+    # Command line argument processing
+    graph = args.mode
+
+    if args.pixel == None:
+        pixel_input = 120
+    else:
+        pixel_input = args.pixel
+
+    if not args.window == None:
+        window_input = int(args.window)
+    else:
+        window_input = 6
+
+    if not args.shift == None:
+        align_amt = args.shift
+    else:
+        align_amt = 0
+
+
+    file_data = open_file(MANDRILL_1)
+
+    # Initialize timestamps
+    # Regularize at the same time
+    motion_timestamps = file_data['motion_timestamps'] - file_data['motion_timestamps'][0]
+    scan_timestamps = file_data['scan_timestamps'] - file_data['scan_timestamps'][0]
+
+    # Import data as variables
+    platform_pos = file_data['platform_pos']
+    scan_data = file_data['scan_data']
+    range_bins = file_data['range_bins']
+    # Set shape
+    scan_data_shape =  file_data['scan_data'].shape
+    # Get the number of scans
+    num_scans = scan_data_shape[0]
+
+    time_ratio = np.max(motion_timestamps) / np.max(scan_timestamps)
+
+    # everything have the same index(same dimensional)
+    ratio = (num_scans * time_ratio) / platform_pos.shape[0]
+    scaled_platform_pos = np.empty((int(num_scans * time_ratio), 3))
+    for i in np.arange(0, 3, 1):
+        scaled_platform_pos[:, i] = np.interp(np.arange(0, int(num_scans * time_ratio), 1) / ratio, np.arange(0, platform_pos.shape[0], 1), platform_pos[:, i])
+
+    motion_timestamps = np.interp(np.arange(0, int(num_scans * time_ratio), 1) / ratio, np.arange(0, motion_timestamps.shape[0], 1), motion_timestamps[:])
+    platform_pos = scaled_platform_pos
+
 
     entry_data = {'scan_data': scan_data, 'platform_pos': platform_pos,
         'range_bins': file_data['range_bins'], 'scan_timestamps': scan_timestamps,
         'motion_timestamps': motion_timestamps, 'corner_reflector_pos': file_data['corner_reflector_pos']
     }
+
+    if args.automatic == True:
+        shift_mode = 'automatic'
+    else:
+        shift_mode = 'manual'
+
+    entry_data = match_filter(entry_data, shift_mode)
+    motion_timestamps = entry_data['motion_timestamps']
+    scan_timestamps = entry_data['scan_timestamps']
+    platform_pos = entry_data['platform_pos']
+    scan_data = entry_data['scan_data']
+    scan_data_shape = scan_data.shape
+    num_scans = scan_data_shape[0]
+    range_bins = entry_data['range_bins']
+
+    print('finished match filtering')
     '''
     print('scan_data.shape:', scan_data.shape)
     print('platform_pos.shape:', platform_pos.shape)
     print('scan_timestamps.shape:', scan_timestamps.shape)
     print('motion_timestamps.shape:', motion_timestamps.shape)
     '''
-    visualize_data(entry_data)
+    #visualize_data(entry_data)
+
     # CROPPING CODE
     # Cut ends off the data
     # in seconds
@@ -329,6 +346,8 @@ def main():
     scan_timestamps =  regularize(scan_timestamps)
     updated_num_scans = scan_data.shape[0]
 
+    print('visualizing entry data')
+    visualize_data(entry_data, 'cropped code')
 
     # Scale data - make everything have the same length M
     # This, however, stll means that the motion data has a different time window than scan_data
@@ -341,12 +360,13 @@ def main():
     motion_timestamps = np.interp(np.arange(0, updated_num_scans, 1) / ratio, np.arange(0, motion_timestamps.shape[0], 1), motion_timestamps[:])
     platform_pos = scaled_platform_pos
 
+
     # Code to make the motion data have the same window as scan data
     time_cut = scan_timestamps[-1]
     index_motion_cut = find_nearest(motion_timestamps, time_cut)
     motion_timestamps = motion_timestamps[:index_motion_cut]
     platform_pos = platform_pos[:index_motion_cut]
-    print('platform_pos.shape:', platform_pos.shape)
+
 
     # Re-scale so the indexes are lined up again
     ratio = updated_num_scans / platform_pos.shape[0]
@@ -356,7 +376,6 @@ def main():
 
     motion_timestamps = np.interp(np.arange(0, updated_num_scans, 1) / ratio, np.arange(0, motion_timestamps.shape[0], 1), motion_timestamps[:])
     platform_pos = scaled_platform_pos
-
 
     # Prepare data entry into backprojection function
     # By this point, all data processing should be completed
@@ -370,7 +389,7 @@ def main():
     if graph == 1:
         print('showing RTI')
         # 1 indicates RTI Plot
-        visualize_data(entry_data)
+        visualize_data(entry_data, 'Final RTI')
     elif graph == 2:
         # 2 indicates Backprojected data
         file_opened = backproject_vectorize_real(entry_data, pixel_input, simulated=True, window=window_input)
---
+++ jimmy_backproj/pulson440/parse_capture_data.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pickle
+import csv
+import sys
+
+MOTION_CAPTURE_FILENAME = sys.argv[1]
+# RADAR_DATA_FILENAME = sys.argv[2]
+PICKLE_DATA_FILENAME = sys.argv[2]
+
+NAME_OF_OBJ = "radar_group6"
+
+# print(MOTION_CAPTURE_FILENAME)
+# print(RADAR_DATA_FILENAME)
+
+ALL_INFO_INDS = []
+POS_INFO_INDS = []
+
+POSITIONS = []
+
+#PARSING POSITION DATA FOR MOCAP
+with open(MOTION_CAPTURE_FILENAME, newline='') as f:
+    reader = csv.reader(f, delimiter=',', quotechar='|')
+    r_ind = 0
+    for row in reader :
+        # print("next rows")
+        for i in range(len(row)) :
+            # print(i)
+            if (r_ind == 3) and (row[i] == NAME_OF_OBJ) :
+                ALL_INFO_INDS.append(i)
+            if (r_ind == 5) and (i in ALL_INFO_INDS) and (row[i] == "Position") :
+                POS_INFO_INDS.append(i)
+        if r_ind >= 7 :
+            POSITIONS.append( ((row[POS_INFO_INDS[0]], row[POS_INFO_INDS[2]], row[POS_INFO_INDS[1]]), row[1]) )
+        r_ind += 1
+        # input()
+        # print(POSITIONS)
+
+with open(PICKLE_DATA_FILENAME, 'wb') as p :
+    print(POSITIONS)
+    pickle.dump(POSITIONS, p)
+
+# #PARSING DATA FOR RADAR
+# def unpack(scan_data_filename):
+#     with open(scan_data_filename, 'rb') as f:
+#         # Read configuration part of data
+#         config = read_config_data(f)
+#         # Compute range bins in datas
+#         scan_start_time = float(config['scan_start'])
+#         start_range = SPEED_OF_LIGHT * ((scan_start_time * 1e-12) - DT_0 * 1e-9) / 2
+#         # Initialize container for unpacked data
+#         data = dict()
+#         data = {'scan_data': [],
+#                 'timestamps': [],
+#                 'pulse_idx': None,
+#                 'range_bins': None,
+#                 'packet_idx': [],
+#                 'config': config}
+#         single_scan_data = []
+#         packet_count = 0
+#         pulse_count = 0
+#         # Read data
+#         while True:
+#             # Read a single data packet and break loop if not a complete packet (in terms of size)
+#             packet = f.read(MAX_SCAN_INFO_PACKET_SIZE)
+#             if len(packet) < MAX_SCAN_INFO_PACKET_SIZE:
+#                 break
+#             # Get information from first packet about how scans are stored and range bins collected
+#             if packet_count == 0:
+#                 num_range_bins = np.frombuffer(packet[44:48], dtype='>u4')[0]
+#                 num_packets_per_scan = np.frombuffer(packet[50:52], dtype='>u2')[0]
+#                 drange_bins = SPEED_OF_LIGHT * T_BIN * 1e-9 / 2
+#                 range_bins = (start_range + drange_bins * np.arange(0, num_range_bins, 1))
+
+#             # Number of samples in current packet, timestamp, and packet index
+#             num_samples = np.frombuffer(packet[42:44], dtype='>u2')[0]
+#             timestamp = np.frombuffer(packet[8:12], dtype='>u4')[0]
+#             data['packet_idx'].append(np.frombuffer(packet[48:50], dtype='>u2')[0])
+
+#             # Extract radar data samples from current packet; process last packet within a scan
+#             # seperately to get all data
+#             packet_data = np.frombuffer(packet[52:(52 + 4 * num_samples)], dtype='>i4')
+#             single_scan_data.append(packet_data)
+#             packet_count += 1
+
+#             if packet_count % num_packets_per_scan == 0:
+#                 data['scan_data'].append(np.concatenate(single_scan_data))
+#                 data['timestamps'].append(timestamp)
+#                 single_scan_data = []
+#                 pulse_count += 1
+
+#         # Add last partial scan if present
+#         if single_scan_data:
+#             single_scan_data = np.concatenate(single_scan_data)
+#             num_pad = data['scan_data'][0].size - single_scan_data.size
+#             single_scan_data = np.pad(single_scan_data, (0, num_pad), 'constant', constant_values=0)
+#             data['scan_data'].append(single_scan_data)
+#             data['timestamps'].append(timestamp)
+#             pulse_count += 1
+
+#         # Stack scan data into 2-D array (rows -> pulses, columns -> range bins)
+#         data['scan_data'] = np.stack(data['scan_data'])
+
+#         # Finalize entries in data
+#         data['timestamps'] = np.asarray(data['timestamps'])
+#         data['pulse_idx'] = np.arange(0, pulse_count)
+#         data['range_bins'] = range_bins
+#         data['packet_idx'] = np.asarray(data['packet_idx'])
+
+#         print(data['scan_data'])
+
+#         return data
+
+
+# #CALCULATING START/END POINTS FOR DATA
+
+
+# #ALIGNING DATA
+
+
+# #RESULT
